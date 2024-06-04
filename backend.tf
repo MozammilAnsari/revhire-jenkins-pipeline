d1f594d3dd3e7c89615ee3857ee657f7523d1e4d
Mozammil Ansari <modassirrr@gmail.com>
2024-06-04 19:57:58 +0530

    final

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-for-revhire-infrastructure-creation"
+    bucket         = "s3-backend-for-revhire-infrastructure-eks-cluster-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "revhire-backend-table"
+    dynamodb_table = "revhire-eks-backend-table"
   }
 }
 
